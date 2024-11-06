584245fb634a1ea69536f0241d200364bb98b12f
Kamil <tagirov2004@bk.ru>
2024-11-07 01:27:55 +0300

    Added Toasts for buttons

--- app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.tagirov.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val settingsButton = findViewById<Button>(R.id.button)
+        val buttonClickListener : View.OnClickListener = object : View.OnClickListener{
+                override fun onClick(v:View?){
+                    Toast.makeText(this@MainActivity, "Нажали на настройки", Toast.LENGTH_SHORT).show()
+                }
     }
+        settingsButton.setOnClickListener(buttonClickListener)
+        val mediaButton = findViewById<Button>(R.id.button2)
+        mediaButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на медиатеку", Toast.LENGTH_SHORT).show()
+        }
+
+        val searchButton = findViewById<Button>(R.id.button3)
+        searchButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на поиск", Toast.LENGTH_SHORT).show()
+        }
+}
 }
