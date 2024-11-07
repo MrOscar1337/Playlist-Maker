21e1ad358339fd366c05a8050e78a5bd8419b878
Kamil <tagirov2004@bk.ru>
2024-11-07 23:40:20 +0300

    Added new activities

--- app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.tagirov.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -23,18 +24,21 @@ class MainActivity : AppCompatActivity() {
         val settingsButton = findViewById<Button>(R.id.button)
         val buttonClickListener : View.OnClickListener = object : View.OnClickListener{
                 override fun onClick(v:View?){
-                    Toast.makeText(this@MainActivity, "Нажали на настройки", Toast.LENGTH_SHORT).show()
+                    val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+                    startActivity(settingsIntent)
                 }
     }
         settingsButton.setOnClickListener(buttonClickListener)
         val mediaButton = findViewById<Button>(R.id.button2)
         mediaButton.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажали на медиатеку", Toast.LENGTH_SHORT).show()
+            val mediaIntent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(mediaIntent)
         }
 
         val searchButton = findViewById<Button>(R.id.button3)
         searchButton.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажали на поиск", Toast.LENGTH_SHORT).show()
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
 }
 }
