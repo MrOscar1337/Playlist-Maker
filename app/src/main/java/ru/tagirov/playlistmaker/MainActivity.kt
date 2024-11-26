8f605f2bf1dcbc5c81b40d85db156294b4fd92e3
MrOscar1337 <tagirov2004@bk.ru>
2024-11-26 21:33:34 +0300

    Added switch for dark theme

--- app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/tagirov/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.tagirov.playlistmaker
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,26 +12,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val layoutRes = getLayoutForTheme()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.light_mode_)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(layoutRes)
 
+        setupUI()
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                setContentView(getLayoutForTheme())
+                setupUI()
+            }
+        }
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(receiver, IntentFilter("com.example.UPDATE_THEME"))
+
+}
+    private fun setupUI(){
         val settingsButton = findViewById<Button>(R.id.button)
         val buttonClickListener : View.OnClickListener = object : View.OnClickListener{
-                override fun onClick(v:View?){
-                    val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
-                    startActivity(settingsIntent)
-                }
-    }
+            override fun onClick(v:View?){
+                val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+            }
+        }
+
         settingsButton.setOnClickListener(buttonClickListener)
+
         val mediaButton = findViewById<Button>(R.id.button2)
         mediaButton.setOnClickListener{
             val mediaIntent = Intent(this@MainActivity, MediaActivity::class.java)
@@ -40,5 +54,12 @@ class MainActivity : AppCompatActivity() {
             val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
             startActivity(searchIntent)
         }
-}
+    }
+
+
+    private fun getLayoutForTheme(): Int {
+        val isDarkMode = getSharedPreferences("AppPrefs", MODE_PRIVATE).getBoolean("DARK_MODE", false)
+        return if (isDarkMode) R.layout.main_dark else R.layout.activity_main
+    }
+
 }
--- app/src/main/java/ru/tagirov/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/tagirov/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,50 @@
 package ru.tagirov.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
+        val layoutRes = getLayoutForTheme()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.settings)
+        setContentView(layoutRes)
+
+
+        // Настройка переключателя темы
+        val themeSwitch = findViewById<Switch>(R.id.themeSwitch)
+        val sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("DARK_MODE", false)
+
+        // Устанавливаем начальное состояние переключателя
+        themeSwitch.isChecked = isDarkMode
+
+        // Слушатель для изменения темы
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            saveThemePreference(isChecked)
+
+            recreate()
+        }
+
+        val intent = Intent("com.example.UPDATE_THEME")
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    private fun getLayoutForTheme(): Int {
+        val isDarkMode = getSharedPreferences("AppPrefs", MODE_PRIVATE).getBoolean("DARK_MODE", false)
+        return if (isDarkMode) R.layout.settings_dark else R.layout.settings
+    }
+
+    private fun saveThemePreference(isDarkMode: Boolean) {
+        val editor = getSharedPreferences("AppPrefs", MODE_PRIVATE).edit()
+        editor.putBoolean("DARK_MODE", isDarkMode)
+        editor.apply()
     }
 }
